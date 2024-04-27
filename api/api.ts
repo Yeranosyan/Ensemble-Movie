c958c978f990f92ce6a63be063bf66e4c540f0bc
Yeranosyan <karlenovich@yahoo.com>
2024-04-26 22:18:19 -0700

    fix issue: requested an insecure XMLHttpRequest endpoint

--- api/api.ts
+++ api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export async function getMovies(title: string) {
-  let apiUrl = `http://www.omdbapi.com/?apikey=${process.env.API_KEY}`;
+  let apiUrl = `https://www.omdbapi.com/?apikey=${process.env.API_KEY}`;
 
   if (title) {
     apiUrl += `&s=${title}`;
